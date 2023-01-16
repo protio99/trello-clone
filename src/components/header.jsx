6e62610c36137940380bbfa217c622c32965edc0
protio99 <dacomu24@gmail.com>
2023-01-16 18:44:37 -0500

    Folder components where we create three files, body, card and header components. The component header it's already done

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__tittle">Trello-clone</h1>
+      <p className="header__paragraph">Using HTML drag and drop API</p>
+    </header>
+  );
+}
